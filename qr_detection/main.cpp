d0214fe5f08099bdc56f501127cdb9b296ee4aad
Анастасия Купоросова <stasy.k@mail.ru>
2018-03-20 13:36:02 +0300

    Namespaces were added

--- qr_detection/main.cpp
+++ qr_detection/main.cpp
@@ -2,61 +2,49 @@
 #include <opencv2/imgproc/imgproc.hpp>  
 #include <zbar.h>  
 #include <iostream>  
-using namespace cv;
-using namespace std;
-using namespace zbar;
+
 
 int main(int argc, char* argv[])
 {
-	VideoCapture cap(0); 
+	cv::VideoCapture cap(0);
 						 
 	if (!cap.isOpened())  
 	{
-		cout << "Cannot open the video cam" << endl;
+		std::cout << "Cannot open the video cam" << std::endl;
 		return -1;
 	}
-	ImageScanner scanner;
-	scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+	zbar::ImageScanner scanner;
+	scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE, 1);
 	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); 
 	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 	
-	Mat frame;
+	cv::Mat frame;
 
 	bool bSuccess = cap.read(frame); 
 	if (!bSuccess) 
 	{
-		cout << "Cannot read a frame from video stream" << endl;
-			
+		std::cout << "Cannot read a frame from video stream" << std::endl;		
 	}
-	Mat grey;
-	cvtColor(frame, grey, CV_BGR2GRAY);
+	cv::Mat grey;
+	cv::cvtColor(frame, grey, CV_BGR2GRAY);
 	int width = frame.cols;
 	int height = frame.rows;
 	uchar *raw = (uchar *)grey.data;
 		
-	Image image(width, height, "Y800", raw, width * height);
+	zbar::Image image(width, height, "Y800", raw, width * height);
 	// scan the image for barcodes   
 	scanner.scan(image);
-	// extract results   
-	for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+	 
+	for (zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
 	{
-		vector<Point> vp;
-		cout << symbol->get_data() << endl;   
+		std::vector<cv::Point> vp;
+		std::cout << symbol->get_data() << std::endl;   
 		int n = symbol->get_location_size();
 		for (int i = 0; i<n; i++)
 		{
-			vp.push_back(Point(symbol->get_location_x(i), symbol->get_location_y(i)));
+			vp.push_back(cv::Point(symbol->get_location_x(i), symbol->get_location_y(i)));
 		}
-		RotatedRect r = minAreaRect(vp);
-		Point2f pts[4];
-		r.points(pts);
-		for (int i = 0; i<4; i++)
-		{
-			line(frame, pts[i], pts[(i + 1) % 4], Scalar(255, 0, 0), 3);
-		}
-			 
-	}
-	  
-	system("pause");
+		symbol->get_location_size();
+	}	 
 	return 0;
 }
