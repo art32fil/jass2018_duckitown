1db119e302b7e70cf5f59d29b7494914efc1d30c
Анастасия Купоросова <stasy.k@mail.ru>
2018-03-22 10:06:09 +0300

    Digits were replaced by strings

--- qr_detection/extract_info.py
+++ qr_detection/extract_info.py
@@ -3,16 +3,16 @@ def extract_info(dec_digit):
     digit = bin(dec_digit)
     array = []
     two_last = digit[-2:]
-    if int(two_last, 2) == 0: array.append(0)
-    elif int(two_last, 2) == 1: array.append(1)
-    elif int(two_last, 2) == 2: array.append(2)
-    else: array.append(3)
+    if int(two_last, 2) == 0: array.append("up")
+    elif int(two_last, 2) == 1: array.append("right")
+    elif int(two_last, 2) == 2: array.append("down")
+    else: array.append("left")
     cross = digit[-5:-2]
-    if int(cross, 2) == 0: array.append(0)
-    elif int(cross, 2) == 1: array.append(1)
-    elif int(cross, 2) == 2: array.append(2)
-    elif int(cross, 2) == 3: array.append(3)
-    else: array.append(4)
+    if int(cross, 2) == 0: array.append("X")
+    elif int(cross, 2) == 1: array.append("T-down")
+    elif int(cross, 2) == 2: array.append("T-left")
+    elif int(cross, 2) == 3: array.append("T-up")
+    else: array.append("T-right")
     num = digit[:-5]
     array.append(int(num, 2))
     array.reverse()
