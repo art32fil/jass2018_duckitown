0aa93029cad18bbc428d3bb71b4631b03e0c980c
trrttrtrsrfewrwed <timurruskiy2@mail.ru>
2018-03-23 16:24:32 +0300

    -1 is the target

--- Duckietown-Software/catkin_ws/src/navigation/src/random_april_tag_turns_node.py
+++ Duckietown-Software/catkin_ws/src/navigation/src/random_april_tag_turns_node.py
@@ -131,7 +131,7 @@ def i_has_unobserved_way(G, id, prev_dir):
     return found_path_to_unobserved(G, id, prev_dir, [], new_possible_moves)
 
 def find_path_to_airport(G, id, prev_dir):
-    if (id == -2):
+    if (id == -1):
        return []
     
 
